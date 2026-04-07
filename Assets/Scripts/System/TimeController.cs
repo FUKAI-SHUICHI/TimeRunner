27ee1ae6a49df6001b302e9e5cec7219aa5642a9
FUKAI-SHUICHI <shuu12018@icloud.com>
2026-04-07 23:26:57 +0900

    メインステージの作成

--- Assets/Scripts/System/TimeController.cs
+++ Assets/Scripts/System/TimeController.cs
@@ -16,18 +16,21 @@ public class TimeController : MonoBehaviour
     public float lerpSpeed = 5f;   // 歪みやFOVの補間速度
 
     [Header("Visual Effects")]
-    public float slowLensIntensity = -50f; // スロー時のLens Distortion
+    public float slowLensIntensity = -0.5f; // スロー時のLens Distortion
     public float normalLensIntensity = 0f; // 通常時のLens Distortion
     public float slowFOV = 80f;            // スロー時のカメラFOV
     public float normalFOV = 60f;          // 通常時のカメラFOV
+    
 
-
+    private ColorAdjustments colorAdjustments;
     private StarterAssetsInputs input;
     private Camera mainCam;
 
     void Start()
     {
         input = FindFirstObjectByType<StarterAssetsInputs>();
+
+
 
         if (volume != null && volume.profile.TryGet(out lens))
         {
@@ -47,11 +50,31 @@ public class TimeController : MonoBehaviour
                 urpCam.renderPostProcessing = true;
         }
 
+        if (volume.profile.TryGet(out colorAdjustments))
+        {
+            colorAdjustments.colorFilter.overrideState = true;
+            colorAdjustments.contrast.overrideState = true;
+        }
+        else
+        {
+            Debug.LogWarning("Color Adjustments が Volume Profile にありません！");
+        }
+
 
     }
 
     void Update()
     {
+        if (input.timeSlow)
+        {
+            Time.timeScale = slowScale;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
         // スロー中かどうかで目標値を切り替え
         float targetLens = input.timeSlow ? slowLensIntensity : normalLensIntensity;
         float targetFOV = input.timeSlow ? slowFOV : normalFOV;
@@ -68,14 +91,30 @@ public class TimeController : MonoBehaviour
             mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, targetFOV, Time.unscaledDeltaTime * lerpSpeed);
         }
 
-        if (input.timeSlow)
-        {
-            Time.timeScale = slowScale;
-        }
-        else
-        {
-            Time.timeScale = 1f;
-        }
-        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        
+
+        Color targetColor = input.timeSlow ? new Color(0.85f, 0.9f, 1f) : Color.white;
+
+        colorAdjustments.colorFilter.value = Color.Lerp(
+            colorAdjustments.colorFilter.value,
+            targetColor,
+            Time.unscaledDeltaTime * 5f
+        );
+
+        float targetExposure = input.timeSlow ? 0.5f : 0f;
+
+        colorAdjustments.postExposure.value = Mathf.Lerp(
+            colorAdjustments.postExposure.value,
+            targetExposure,
+            Time.unscaledDeltaTime * 5f
+        );
+
+        float targetContrast = input.timeSlow ? -10f : 0f;
+
+        colorAdjustments.contrast.value = Mathf.Lerp(
+            colorAdjustments.contrast.value,
+            targetContrast,
+            Time.unscaledDeltaTime * 5f
+        );
     }
 }
